9326e547526d7c1cded005aabcf478f1db49757c
jimmyhu213 <jiemingh213@gmail.com>
2026-05-01 12:45:46 +1000

    feat: add smooth page transition fade between routes

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
+import { PageTransition } from "./components/page-transition";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function FrontendLayout({
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<Header />
 				<main className="min-h-screen">
-					{children}
+					<PageTransition>{children}</PageTransition>
 				</main>
 				<Footer />
 			</body>
